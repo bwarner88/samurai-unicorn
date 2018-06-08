57aec026da300c116b79e1bfc9097549a1b27ea6
Greatest Dane <DaneGoodman85@gmail.com>
2018-06-07 20:12:24 -0700

    API linked, submit button linked, ready for Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
-var url = "https://www.giantbomb.com/api/search/?api_key=4a12e90d2bea50d175659d20cfed7dd6425d84a3&format=jsonp&query=star-craft&resources=game";
+var gameName = $("#game-input").val();
+
+var url = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
 var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
 
+//Create on click event to search for a video game
+$("#search-button").on("click", function(event){
+    var p = $("<p>");
+    $(".lead").append(p.text("this is a test"));
+    console.log("test");
 
 $.ajax({
     url: url,
@@ -17,4 +24,6 @@ $.ajax({
     error: function(error) {
         console.log(error);
     }
-})
+});
+
+});
