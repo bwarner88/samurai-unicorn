b72910a31a7f985c35acafecadf44be3520f6963
Raymond <rs01247@gmail.com>
2018-06-09 11:59:32 -0700

    url parameter passes to next page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,71 @@
-var gameName;
-var resultPage;
+$(document).ready(function(){
 
-//Create on click event to search for a video game
-$("#search-button").on("click", function (event) {
-    event.preventDefault()
-    gameName = $("#game-input").val().trim();
-    resultPage = "./gamepage.html?search=" + gameName;
-    window.location.href = resultPage;
+    function getUrlParams(prop) {
+        var params = {};
+        var search = decodeURIComponent(location.href.slice(location.href.indexOf('?') + 1));
+        var definitions = search.split('&');
 
-});
+        definitions.forEach(function (val, key) {
+            var parts = val.split('=', 2);
+            params[parts[0]] = parts[1];
+        });
 
-function giantBomb() {
+        return (prop && prop in params) ? params[prop] : params;
+    }
+    
+    var userParam = getUrlParams(location.href);
+    console.log(userParam);
+    var gameName;
+    var resultPage = "./gamepage.html?search=" + userParam;
 
-    console.log("Hello");
+    
+    //Create on click event to search for a video game
+    $("#search-button").on("click", function (event) {
+        event.preventDefault()
+        gameName = $("#game-input").val().trim();
+        location.href = "./gamepage.html?search=" + gameName;
 
-    // Giant Bomb API Info
-    var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
-    var giantbombURL = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
-    console.log(gameName)
-    console.log(giantbombURL);
-
-    // var p = $("<p>");
-    // $(".lead").append(p.text("this is a test"));
-    // console.log("test");
-
-    // Button click leads to gamepage.html
-    // URL parameter to add "?S" to URL 
-    // window.location
-
-    // NEED A LOADING PAGE (spinner) for AJAX to run
-    // YOUTUBE
-
-    $.ajax({
-        url: giantbombURL,
-        method: 'GET',
-        dataType: 'jsonp',
-        crossDomain: true,
-        jsonp: 'json_callback',
-        complete: function () {
-            console.log("done")
-        },
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (error) {
-            console.log(error);
-        }
     });
-}
 
-//ES6 FOR GAMEPAGE.HTML?SEARCH=GAMENAME
-if (window.location.href === resultPage) {
-    // giantBomb();
-    console.log("Yo");
-}
+    function giantBomb() {
 
-// window.onload = function () {
-//     if (window.location.href.indexOf(resultPage) > -1) {
-//         console.log("Yo");
-//     }
-// }
+        console.log("Hello");
 
-//Twitch Stream API Info
-// var httpRequest = new XMLHttpRequest();
-// axios({
-//     url: "https://api.twitch.tv/kraken/clips/top?limit=2&game=overwatch&trending=true",
-//     headers: {
-//         "Client-ID": "7wqn4lbccr164m3mxkpoxfxvc9tso7",
-//         'Accept': 'application/vnd.twitchtv.v5+json'
-//     },
-//     method: 'GET',
-// })
-//     .then(function (response) {
-//         console.log(response.data)
-//         console.log("baxios")
-//         clipList = response.data;
-//         var clipsDisplay = document.getElementById('clips-display');
-//         console.log(clipList)
-//         clipList.clips.forEach(function (clip, index, array) {
-//             clipItem = document.createElement('div');
-//             clipItem.innerHTML = clip.embed_html;
-//             clipsDisplay.appendChild(clipItem);
-//         })
-//     })
-//     .catch(function (err) {
-//         console.error(err)
-//     })
+        // Giant Bomb API Info
+        var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
+        var gbURL = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
+        console.log(gameName)
+        console.log(gbURL);
+
+        // Button click leads to gamepage.html
+        // URL parameter to add "?S" to URL 
+        // location
+
+        // NEED A LOADING PAGE (spinner) for AJAX to run
+        // YOUTUBE
+
+        $.ajax({
+            url: gbURL,
+            method: 'GET',
+            dataType: 'jsonp',
+            crossDomain: true,
+            jsonp: 'json_callback',
+            complete: function () {
+                console.log("done")
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+    //ES6 FOR GAMEPAGE.HTML?SEARCH=GAMENAME
+    if (location.href === resultPage) {
+        // giantBomb();
+        console.log("Yo");
+    }
+
+})
