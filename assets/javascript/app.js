53fe7a7f47f06eb63f3162fe2c8da3b4f82a919d
unknown <brian_warner25@yahoo.com>
2018-06-09 10:45:08 -0700

    pulling origin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,17 @@
 var gameName = $("#game-input").val();
 
 var giantbombURL = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
+
 var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
 
-//Create on click event to search for a video game
+// Create on click event to search for a video game
 $("#search-button").on("click", function(event){
     var p = $("<p>");
     $(".lead").append(p.text("this is a test"));
     console.log("test");
 
 $.ajax({
-    url: url,
+    url: giantbombURL,
     method: 'GET',
     dataType: 'jsonp',
     crossDomain: true,
@@ -28,6 +29,7 @@ $.ajax({
 
 });
 
+
 var httpRequest = new XMLHttpRequest();
 axios({
     url: "https://api.twitch.tv/kraken/clips/top?limit=2&game=overwatch&trending=true",
@@ -37,7 +39,6 @@ axios({
     },
     method: 'GET',
 })
-<<<<<<< HEAD
     .then(function (response) {
         console.log(response.data)
         console.log("baxios")
@@ -53,20 +54,3 @@ axios({
     .catch(function (err) {
         console.error(err)
     })
-=======
-.then(function(response){
-console.log(response.data)
-console.log("baxios")
-clipList = response.data;
-var clipsDisplay = document.getElementById('clips-display');
-console.log(clipList)
-clipList.clips.forEach(function(clip, index, array) {
-clipItem = document.createElement('div');
-clipItem.innerHTML = clip.embed_html;
-clipsDisplay.appendChild(clipItem);
-})
-})
-.catch(function(err){
-console.error(err)
-})
->>>>>>> 79c93649aa1ef4b3d60cec4eafb1bd4f75441632
