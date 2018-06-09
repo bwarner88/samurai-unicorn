749256eb8ab5ac320e0e5ed16536b6594aeae567
Greatest Dane <DaneGoodman85@gmail.com>
2018-06-09 10:24:55 -0700

    I've got all the game results displayed to the gamepage.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,69 @@
-var gameName = $("#game-input").val();
 
-var url = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
-var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
+
+
+
 
 //Create on click event to search for a video game
-$("#search-button").on("click", function(event){
-    var p = $("<p>");
-    $(".lead").append(p.text("this is a test"));
-    console.log("test");
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    
+    for (i = 0; i < 9; i ++) {
+    $(".game-display" + i).empty();
+    };
 
-$.ajax({
-    url: url,
-    method: 'GET',
-    dataType: 'jsonp',
-    crossDomain: true,
-    jsonp: 'json_callback',
-    complete: function() {
-        console.log("done")
-    },
-    success: function(data) {
-        console.log(data);
-    },
-    error: function(error) {
-        console.log(error);
-    }
+    var gameName = $("#game-input").val();
+    var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
+    var gbURL = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + gameName + "&resources=game";
+    
+
+    $.ajax({
+        url: gbURL,
+        method: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        complete: function () {
+            console.log("done")
+        },
+        success: function (data) {
+            console.log(data);
+            console.log(data.results[0].image.screen_large_url);
+            console.log(data.results[0].original_game_rating[0].name)
+            
+            for (var i = 0; i < 9; i ++) {
+            
+            var rating = $("<h3>");
+            rating.text(data.results[i].original_game_rating[0].name);
+            $(".game-display" + i).prepend(rating);
+
+            var platforms = $("<h3>");
+            platforms.text(data.results[i].platforms[0].name);
+            $(".game-display" + i).prepend(platforms);
+
+            var info = $("<p>");
+            info.text(data.results[i].deck);
+            $('.game-display' + i).prepend(info);
+
+            var img = $("<img>");
+            img.attr("id", "game-image");
+            img.attr("class", "image-click");
+            img.attr("value", data.results[i].name);
+            img.attr("src", data.results[i].image.original_url);
+            $(".game-display" + i).append(img);
+
+            };
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    $("#game-input").val("");
+
 });
 
-});
+$(document).on("click", ".image-click", function(){
+    console.log($(this).val())
+})
+
+
