06af4a28902d242ff823d23b6a486fde7d7cacb8
Greatest Dane <DaneGoodman85@gmail.com>
2018-06-11 18:22:38 -0700

    Results page is all finished up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,27 +42,26 @@ $(document).ready(function () {
 
                 for (var i = 0; i < 9; i++) {
 
-                    var rating = $("<h3>");
-                    rating.text(data.results[i].original_game_rating[0].name);
-                    $(".game-display" + i).prepend(rating);
-
-                    var platforms = $("<h3>");
-                    platforms.text(data.results[i].platforms[0].name);
-                    $(".game-display" + i).prepend(platforms);
-
-                    var info = $("<p>");
-                    info.text(data.results[i].deck);
-                    $('.game-display' + i).prepend(info);
-                    $('#results-game-display').prepend(info);
-
-
                     var img = $("<img>");
                     img.attr("id", "game-image");
                     img.attr("class", "image-click");
                     img.attr("value", data.results[i].name);
                     img.attr("src", data.results[i].image.original_url);
                     $(".game-display" + i).append(img);
+                   
+                    var platforms = $("<h3>");
+                    platforms.text(data.results[i].platforms[0].name);
+                    $(".game-display" + i).append(platforms);
 
+                    var rating = $("<h3>");
+                    rating.text(data.results[i].original_game_rating[0].name);
+                    $(".game-display" + i).append(rating);
+
+                    var info = $("<p>");
+                    info.attr("card-text");
+                    info.text(data.results[i].deck);
+                    $('.game-display' + i).append(info);
+                
                 };
             },
             error: function (error) {
@@ -74,7 +73,7 @@ $(document).ready(function () {
     }
 
     //USER INPUT GAME SEARCH ON MAIN PAGE, CLICKING SEARCH SAVES VALUE OF INPUT
-    $("#start-button").on("click", function (event) {
+    $("#start-button, #search-button").on("click", function (event) {
         event.preventDefault()
         gameName = $("#game-input").val().trim();
         location.href = "./gamepage.html?search=" + gameName;
@@ -88,11 +87,49 @@ $(document).ready(function () {
     //CHECK TO SEE WE'RE ON RESULTS PAGE
     if (whatPage === 'result') {
         twitchDisplay();
+
+        //Create a custom API call to Giant-BOmb for the final results page
+        var gbAPI = "4a12e90d2bea50d175659d20cfed7dd6425d84a3"
+        var gbURL = "https://www.giantbomb.com/api/search/?api_key=" + gbAPI + "&format=jsonp&query=" + userParam.search + "&resources=game";
+        console.log(gbURL);
+
+        $.ajax({
+            url: gbURL,
+            method: 'GET',
+            dataType: 'jsonp',
+            crossDomain: true,
+            jsonp: 'json_callback',
+            complete: function () {
+                console.log("done")
+            },
+            success: function (data) {
+                console.log(data);
+                console.log(data.results[0].name);
+
+                var name = $("<h2>");
+                    name.text(data.results[0].name);
+                    $("#game-name").append(name);
+
+                var img = $("<img>");
+                    img.attr("id", "game-image");
+                    img.attr("src", data.results[0].image.original_url);
+                    $("#game-results-display").append(img);
+
+                var description = $("<div>");
+                    description.html(data.results[0].description);
+                    $("#description").append(description);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+        
+
     }
 
 
     //Create on click event to search for a video game
-    $("#search-button").on("click", function (event) {
+    $("#search-button", "#start-button").on("click", function (event) {
         event.preventDefault();
         giantBomb();
 
@@ -116,11 +153,12 @@ $(document).ready(function () {
             crossDomain: true,
             jsonp: 'json_callback',
             complete: function () {
-                console.log("done")
+                console.log("done for real")
+
             },
             success: function (data) {
                 console.log(data);
-                console.log(data.results[0].name);
+                console.log("giant-bomb");
 
             }
         });
@@ -129,31 +167,31 @@ $(document).ready(function () {
 
 
     function twitchDisplay() {
-        var httpRequest = new XMLHttpRequest();
-        axios({
-            url: "https://api.twitch.tv/kraken/clips/top?limit=2&game=" + userParam.search + "&trending=true",
-            headers: {
-                "Client-ID": "7wqn4lbccr164m3mxkpoxfxvc9tso7",
-                'Accept': 'application/vnd.twitchtv.v5+json'
-            },
-            method: 'GET',
-        })
+    var httpRequest = new XMLHttpRequest();
+    axios({
+        url: "https://api.twitch.tv/kraken/clips/top?limit=2&game=" + userParam.search + "&trending=true",
+        headers: {
+            "Client-ID": "7wqn4lbccr164m3mxkpoxfxvc9tso7",
+            'Accept': 'application/vnd.twitchtv.v5+json'
+        },
+        method: 'GET',
+    })
 
-            .then(function (response) {
-                console.log(response.data)
-                console.log("baxios")
-                clipList = response.data;
-                var clipsDisplay = document.getElementById('clips-display');
-                console.log(clipList)
-                clipList.clips.forEach(function (clip, index, array) {
-                    clipItem = document.createElement('div');
-                    clipItem.innerHTML = clip.embed_html;
-                    clipsDisplay.appendChild(clipItem);
-                })
+        .then(function (response) {
+            console.log(response.data)
+            console.log("baxios")
+            clipList = response.data;
+            var clipsDisplay = document.getElementById('clips-display');
+            console.log(clipList)
+            clipList.clips.forEach(function (clip, index, array) {
+                clipItem = document.createElement('div');
+                clipItem.innerHTML = clip.embed_html;
+                clipsDisplay.appendChild(clipItem);
             })
-            .catch(function (err) {
-                console.error(err)
-            })
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
     }
 
 })
